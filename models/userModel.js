f2425a19278cdf6c75a1ac16e6f1bd9e5943b214
PavinKM <pavinyagava@gmail.com>
2025-06-28 00:16:37 +0530

    validation

--- models/userModel.js
+++ models/userModel.js
@@ -7,9 +7,24 @@ const addressSchema = new mongoose.Schema({
 
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number,
-    email: String,
-    createdAt: Date,
+    age: {
+        type: Number,
+        min: 10,
+        max: 20,
+        validate : {
+            validator: v => v % 2 == 0,
+            message: props => `${props.value} is not number`
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now()
+    },
     updatedAt: Date,
     bestFriend: mongoose.SchemaTypes.ObjectId,
     hobbies: [String],
